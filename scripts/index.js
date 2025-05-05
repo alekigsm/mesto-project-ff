141e6c3d8b09446de73d152a5e4a05f39589686b
Aleksandr Smirnov <igr0k@yandex.ru>
2025-05-05 23:55:51 +0300

    частично сделал

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list')
 
 // @todo: Функция создания карточки
+function addCard ( cardTitle = "затычкатайтла" ,cardImage = "#"){
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+cardElement.querySelector('.card__title').textContent = cardTitle;
+cardElement.querySelector('.card__image').src = cardImage;
+placesList.append(cardElement)
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(card) {
+  addCard(card.name, card.link); // Передаём image и title из объекта
+});
