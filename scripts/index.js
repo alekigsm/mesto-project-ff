5a64a012d73f8cdaf35e9a7913f28603248c56b8
Aleksandr Smirnov <igr0k@yandex.ru>
2025-05-10 16:51:00 +0300

    prettier

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,31 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
-
 // @todo: DOM узлы
 const content = document.querySelector('.content');
 const placesList = content.querySelector('.places__list');
 
-
 // @todo: Функция создания карточки
-function addCard ( cardTitle = "затычкатайтла" ,cardImage = "#", deleteCallback){
-const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-cardElement.querySelector('.card__title').textContent = cardTitle;
-cardElement.querySelector('.card__image').src = cardImage;
-cardElement.querySelector('.card__delete-button').addEventListener('click', function(evt) {
-  deleteCallback(cardElement); 
-});
+function addCard(cardTitle = 'затычкатайтла', cardImage = '#', deleteCallback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = cardTitle;
+  cardElement.querySelector('.card__image').src = cardImage;
+  cardElement
+    .querySelector('.card__delete-button')
+    .addEventListener('click', function (evt) {
+      deleteCallback(cardElement);
+    });
 
-return cardElement;
-};
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
-function deleteCard(cardElement){
+function deleteCard(cardElement) {
   cardElement.remove();
 }
 
-
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function(card) {
+initialCards.forEach(function (card) {
   const cardElement = addCard(card.name, card.link, deleteCard);
   placesList.append(cardElement);
 });
-
