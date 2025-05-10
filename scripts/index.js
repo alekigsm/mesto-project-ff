3009d068a264cd4c8e0217cd417c085a84494efb
Aleksandr Smirnov <igr0k@yandex.ru>
2025-05-10 16:36:49 +0300

    ok

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,30 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const content = document.querySelector('.content');
-const placesList = content.querySelector('.places__list')
+const placesList = content.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
-function addCard ( cardTitle = "затычкатайтла" ,cardImage = "#"){
+function addCard ( cardTitle = "затычкатайтла" ,cardImage = "#", deleteCallback){
 const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 cardElement.querySelector('.card__title').textContent = cardTitle;
 cardElement.querySelector('.card__image').src = cardImage;
-placesList.append(cardElement)
-}
+cardElement.querySelector('.card__delete-button').addEventListener('click', function(evt) {
+  deleteCallback(cardElement); 
+});
+
+return cardElement;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement){
+  cardElement.remove();
+}
+
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(card) {
-  addCard(card.name, card.link); // Передаём image и title из объекта
+  const cardElement = addCard(card.name, card.link, deleteCard);
+  placesList.append(cardElement);
 });
+
